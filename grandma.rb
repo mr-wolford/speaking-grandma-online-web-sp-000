eafa6acf652082471451766ec1d54ed5fde2d837
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-06 21:35:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase)
-  if !!phrase.upcase
+  if phrase == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+  elsif !!phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK, UP SONNY!"
   end
